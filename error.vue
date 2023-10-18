dfc816bb456ccff001a2ace6da8cf07c41289db5
efetivos <victor.efetivos@gmail.com>
2023-10-18 05:04:15 -0400

    Done: [ 404 ] Page

---
+++ error.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <MenuFixed/>
+	<div data-page="404" class="page page-404">
+        <div class="error">
+            <div class="error__content">
+                <div class="error__texts">
+                    <h1 class="error__title">404</h1>
+                    <p class="error__text">
+                        <span class="error__subt">Something went wrong</span>
+                        <span class="error__wrong">Page not found</span>
+                    </p>
+                </div>
+                <nuxt-link href="/" class="error__link">Go back home</nuxt-link>
+            </div>
+        </div>
+	</div>
+</template>
+<script setup>
+    useHead({
+        title: 'Page not found | VWLAB Slides',
+        bodyAttrs: {
+            class: 'index'
+        }
+    })
+</script>
+
+<script>
+	import { transitions } from "assets/js/utils_global/transitions";
+	import { Console } from "assets/js/utils_global/console";
+
+	export default {
+		mounted() {
+			const $current = document.querySelector('.page')
+			transitions.firstEntrance()
+			transitions.instancingPages($current, $current.dataset.page)
+			new Console()
+		}
+	}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,6 @@
 <template lang="pug">
 div(data-page="index" class="page page-index index ")
 
-    //- template(v-for=" post in data.chapters_list")
-    //-     p {{ post.chapter.uid }}
-    h1 {{ data.title }}
-
-    ul
-        template(v-for=" post in data.chapters_list")
-            template(v-for=" post_list in slides")
-                li(v-if="post.chapter.uid === post_list.uid")
-                        //- NuxtLink(:to="`/chapter/${post_list.uid}`") {{ post_list.data.page_title }}
-
-    //- template(v-for="(slice, index) in data.body")
-    //-     h1 - {{ slice.slice_type }}
 </template>
 
 <script setup>
